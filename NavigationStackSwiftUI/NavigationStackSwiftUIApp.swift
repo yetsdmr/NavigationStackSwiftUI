eae4d46d9adfe789511810a8dfc377bb3b78a39b
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-04-17 14:42:42 +0300

    Initial Commit

---
+++ NavigationStackSwiftUI/NavigationStackSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationStackSwiftUIApp.swift
+//  NavigationStackSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 17.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NavigationStackSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
