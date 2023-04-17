7b623dc9366997149d19b71c0f5954b4fe513618
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-04-17 16:16:53 +0300

    Added Navigation Stack.

--- NavigationStackSwiftUI/ContentView.swift
+++ NavigationStackSwiftUI/ContentView.swift
@@ -7,15 +7,104 @@
 
 import SwiftUI
 
+struct CarBrand: Identifiable, Hashable{
+    let name : String
+    let id = NSUUID().uuidString
+}
+
+struct Car: Identifiable, Hashable{
+    let id = NSUUID().uuidString
+    let make : String
+    let model : String
+    let year : Int
+    
+    var description: String {
+        return "\(year) \(make) \(model)"
+    }
+}
+
 struct ContentView: View {
+    let brands: [CarBrand] = [
+        .init(name: "Ferrari"),
+        .init(name: "Lamborghini"),
+        .init(name: "Mercedes"),
+        .init(name: "Aston Martin")
+    ]
+    
+    let cars: [Car] = [
+        .init(make: "Ferrari", model: "488", year: 2022),
+        .init(make: "Lamborghini", model: "Aventador", year: 2023),
+        .init(make: "Mercedes", model: "AMG 63", year: 2019),
+        .init(make: "Aston Martin", model: "Vantage", year: 2021)
+    ]
+    
+    @State private var navigationPath = [CarBrand]()
+    @State private var showFullStack = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack(path: $navigationPath) {
+            VStack {
+                List {
+                    Section("Manufacturers") {
+                        ForEach(brands) { brand in
+                            NavigationLink(value: brand) {
+                                Text(brand.name)
+                            }
+                        }
+                    }
+                    
+                    Section("Cars") {
+                        ForEach(cars) { car in
+                            NavigationLink(value: car) {
+                                Text(car.description)
+                            }
+                        }
+                    }
+                }
+                .navigationDestination(for: CarBrand.self) { brand in
+                    VStack {
+                        viewForBrand(brand)
+                        
+                        Button {
+                            navigationPath = []
+                        } label: {
+                            Text("Go to root")
+                        }
+
+                    }
+                }
+                .navigationDestination(for: Car.self) { car in
+                    Color(.systemRed)
+                }
+                
+                Button {
+                    if showFullStack {
+                        navigationPath = brands
+                    } else {
+                        navigationPath = [brands[0], brands[2], brands[1]]
+                    }
+                } label: {
+                    Text("View All")
+                }
+
+            }
         }
-        .padding()
+        
+    }
+    
+    func viewForBrand(_ brand: CarBrand) -> AnyView {
+        switch brand.name {
+        case "Ferrari":
+            return AnyView(Color(.systemRed))
+        case "Lamborghini":
+            return AnyView(Color(.systemPurple))
+        case "Mercedes":
+            return AnyView(Color(.systemOrange))
+        case "Aston Martin":
+            return AnyView(Color(.systemGreen))
+        default:
+            return AnyView(Color(.systemGreen))
+        }
     }
 }
 
